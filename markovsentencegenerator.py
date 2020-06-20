eb5d8e3564fc8fb0fc473f704ebfd3465d692b10
XAPATHEIA <54541033+XAPATHEIA@users.noreply.github.com>
2020-06-20 18:03:36 +0100

    accurately simulates markov chain
    uses word positions to construct a markov chain - keep getting index error, WIP

--- markovsentencegenerator.py
+++ markovsentencegenerator.py
@@ -1,8 +1,8 @@
 import requests
 import pyperclip
-import re
 import collections
 import random
+import string
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
@@ -17,94 +17,99 @@ while True:
 # Grabbing user's URL from the
 base_url = pyperclip.paste()
 
-try:
-    response = requests.get(base_url)
 
-    # If the response was successful, no Exception will be raised.
-    response.raise_for_status()
+response = requests.get(base_url)
 
-    # Using BeautifulSoup to parse the HTML contents of the provided URL.
-    initial_soup = BeautifulSoup(response.content, 'html.parser')
+# If the response was successful, no Exception will be raised.
+response.raise_for_status()
 
-    # Kill all script and style elements
-    for script in initial_soup(["script", "style"]):
-        script.extract()  # Rip it out
+# Using BeautifulSoup to parse the HTML contents of the provided URL.
+initial_soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Grabbing only the text in the URL.
-    contents = initial_soup.get_text()
+# Kill all script and style elements
+for script in initial_soup(["script", "style"]):
+    script.extract()  # Rip it out
 
-    # Break into lines and remove leading and trailing space on each.
-    lines = (line.strip() for line in contents.splitlines())
-    # Break multi-headlines into a line each
-    tertiary_text = list((phrase.strip() for line in lines for phrase in line.split("  ")))
+# Grabbing only the text in the URL.
+contents = initial_soup.get_text()
 
-    # Regex file to only hold characters between a-zA-Z
-    characters = re.compile('[a-zA-Z]')
+# Break into lines and remove leading and trailing space on each.
+lines = (line.strip() for line in contents.splitlines())
+# Break multi-headlines into a line each
+tertiary_text = list((phrase.strip() for line in lines for phrase in line.split("  ")))
 
 
-    def punctuation_strip(lst):
-        stripped_punctuation_list = []
-        for item in lst:
-            if characters.findall(item):
-                stripped_punctuation_list.append(item)
-            else:
-                continue
-        return stripped_punctuation_list
+def punctuation_strip(lst):
+    stripped_punctuation_list = []
+    for item in lst:
+        stripped_punctuation_list.append(item.strip(string.punctuation))
+    return stripped_punctuation_list
+
+secondary_text = punctuation_strip(tertiary_text)
+
+# Final list containing every single word from the provided URL.
+def convert(provided_list):
+    primary_list = []
+    temporary_list = []
+    for sentence in provided_list:
+        temporary_list = sentence.split()
+        for word in temporary_list:
+            primary_list.append(word)
+    return primary_list
+
+primary_preliminary_list = convert(secondary_text)
+primary_text = []
+for item in primary_preliminary_list:
+    primary_text.append(item.strip(string.punctuation))
+
+# Creating a set in order to remove duplicates so as to allow for reliable root word selection.
+unique_set = set(primary_text)
+
+# Randomly selecting initial root word so as to allow diversity in generated sentences.
+root_word = random.choice(tuple(unique_set))
+
+generated_sentence = root_word.capitalize()
+
+random_sentence_length = random.randint(5, 20)
+for iteration in range(random_sentence_length):
+    # Locating the index of the words that come AFTER the root word.
+    indices = [i + 1 for i, x in enumerate(primary_text) if x == root_word]
+
+    # Turning the indices into strings so that they can be counted.
+    following_words_raw = []
+    for k in indices:
+        following_words_raw.append(primary_text[k])
+
+    # Constructing probability distribution.
+    probability_distribution = []
+    u_following_words = list(set(following_words_raw))
+    for word in u_following_words:
+        probability_distribution.append(round(following_words_raw.count(word) / len(following_words_raw), 10))
+    print(u_following_words)
+    print(probability_distribution)
+
+    if len(probability_distribution) == 1:
+        root_word = u_following_words[0]
+    else:
+        root_word = random.choices(
+            population=u_following_words,
+            weights=probability_distribution,
+            k=1)
+
+    a = punctuation_strip(root_word)
+    if len(a) == 0:
+        print(generated_sentence + ".")
+    elif iteration == random_sentence_length:
+        generated_sentence += " " + a[0] + "."
+    else:
+        generated_sentence += " " + a[0]
+
+print(generated_sentence)
 
 
-    secondary_text = punctuation_strip(tertiary_text)
-
-    # Final list containing every single word from the provided URL.
-    def convert(provided_list):
-        primary_list = []
-        temporary_text = []
-        for sentence in provided_list:
-            temporary_list = sentence.split()
-            for word in temporary_list:
-                primary_list.append(word)
-        return primary_list
-
-
-    primary_preliminary_list = convert(secondary_text)
-    primary_text = punctuation_strip(primary_preliminary_list)
-
-    # Constructing probabilities.
-    words_and_prob = (collections.Counter(primary_text)).most_common()
-    total_n_words = len(primary_text)
-    probabilities = []
-    for i in range(len(words_and_prob)):
-        probabilities.append(float("{:.10f}".format(words_and_prob[i][1]/total_n_words)))
-    # Sorting probability list so it can map accurately to counter.
-    probabilities.sort(reverse=True)
-
-    # Creating a function to check if first letter is uppercase in a randomly selected dictionary
-    def is_uppercase(word_dict):
-        while True:
-            if (a := random.choice(primary_text))[0].isupper() and len(a) >= 4:
-                break
-            else:
-                continue
-        return a
-
-    # Creating the lead word.
-    lead_word = is_uppercase(words_and_prob)
-    words = []
-    for k in range(len(words_and_prob)):
-        words.append(words_and_prob[k][0])
-
-    # Generating sentence
-    list_of_generated_words = random.choices(
-        population=words,
-        weights=probabilities,
-        k=random.randint(10, 20)
-    )
-
-    generated_sentence = lead_word + ' ' + ' '.join(punctuation_strip(list_of_generated_words)) + "."
-    print(generated_sentence)
-
-
-except HTTPError as http_err:
+"""except HTTPError as http_err:
     print(f"HTTP error occurred: {http_err}")
 except Exception as err:
-    print(f"Other error occurred: {err}")
+    print(f"Other error occurred: {err}")"""
+
 
